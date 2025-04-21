9866c98266df24ae05d4fa6281ac192743adbfca
DavidBcyber <dbustos28@amarillocollege.com>
2025-04-20 22:58:40 -0500

    Update Program.cs

--- Week 2-1/Program.cs
+++ Week 2-1/Program.cs
@@ -10,24 +10,24 @@ namespace Week_2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Interest Calculator Program");
+            Console.WriteLine("Interest Calculator Program"); //Calculator
 
-            double principal, rate, time, Intreast;
+            double principal, rate, time, Intreast; // double
 
-            Console.WriteLine("What is princaipal amount of the loan in dollars?");
+            Console.WriteLine("What is princaipal amount of the loan in dollars?"); // the amount of dollars
             principal = Convert.ToDouble(Console.ReadLine());
 
 
-            Console.WriteLine("What is intrest rate (input 0.05 for 5%)?");
+            Console.WriteLine("What is intrest rate (input 0.05 for 5%)?");// the intrest rate
             rate = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("What is period of loan in years?");
+            Console.WriteLine("What is period of loan in years?"); // loan period
             time = Convert.ToDouble(Console.ReadLine());
 
 
-            Console.WriteLine(Intreast = principal * rate * time);
+            Console.WriteLine(Intreast = principal * rate * time); // the calculation
 
-            Console.WriteLine("Total intrest of loan is:" + Intreast + " dollars");
+            Console.WriteLine("Total intrest of loan is:" + Intreast + " dollars"); // the total of the intreast
         }
     }
 }
